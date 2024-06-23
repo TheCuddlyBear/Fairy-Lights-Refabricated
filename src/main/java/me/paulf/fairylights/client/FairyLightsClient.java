bb57e4c8db12a5a5fd2d4e5bf017f6f7d96941ae
TheCuddlyBear <basvthazeveld@outlook.com>
2024-06-23 21:21:55 +0200

    Fabric related files

---
+++ src/main/java/me/paulf/fairylights/client/FairyLightsClient.java
@@ -0,0 +1,10 @@
+package me.paulf.fairylights.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FairyLightsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
